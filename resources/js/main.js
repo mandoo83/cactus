e499ac47df7a586de5038f9063d1005057f5c0ae
mandoo83 <manapain83@gmail.com>
2021-04-30 16:25:19 +0900

    수정

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,45 @@
+// fullpage
+$("#fullpage").fullpage({
+    onLeave: function (origin, destination, direction) {
+        if (origin == 2 && direction == "down") {
+            $(".section").eq(1).addClass("v1");
+        } else if (origin == 3 && direction == "up") {
+            $(".section").eq(1).addClass("v1");
+        } else {
+            $(".v1").removeClass("v1");
+        }
+    },
+    afterLoad: function (anchorLink, origin) {
+        if (origin == 4 || origin == 5) {
+            $("header").addClass("black");
+        } else {
+            $("header").removeClass("black");
+        }
+    }
+});
+
+// tab 
+var idx = 0;
+
+$(".tabNav .tab li").on("click", function () {
+    var _this = $(this);
+    idx = _this.index();
+    _this.addClass("on").siblings().removeClass("on");
+    $(".tabNav .ment li").eq(idx).addClass("on").siblings().removeClass("on");
+    $(".tabBody ul").eq(idx).addClass("on").siblings().removeClass("on");
+    $(".tabBody ul").eq(idx).find("li:first-child").addClass("on").siblings().removeClass("on");
+    $(".section:eq(2) .bg img").eq(idx * 3).addClass("on").siblings().removeClass("on");
+});
+
+$(".tabBody li").on("mouseenter", function () {
+    var _this = $(this);
+    var idx2 = _this.index();
+    $(this).addClass("on").siblings().removeClass("on");
+    $(".section:eq(2) .bg img").eq((idx * 3) + idx2).addClass("on").siblings().removeClass("on");
+});
+
+
+/*
 var contents = new Swiper(".contents", {
     direction: "vertical",
     mousewheel: true
@@ -82,3 +124,5 @@ var setTimeoutFnc = function () {
     $(".tabWrap #toBg").removeClass(classStr);
     fromClass = toClass;
 };
+
+*/
